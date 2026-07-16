2b87ca5f847aa60941bec20520e14c7095663d81
Aldo-Canegalli <aldo.lucciano.canegalli@gmail.com>
2026-07-15 21:32:26 -0500

    Fix API URL for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,7 +9,10 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+});
